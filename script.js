87f134af6a7365cb4c84efb6e6a257f517df1c8e
subcylindrical <zacharylehouillier@gmail.com>
2023-02-24 12:39:39 -0800

    implement paddle movement

--- script.js
+++ script.js
@@ -4,10 +4,24 @@ const closeRulesBtn = document.querySelector('.close-btn');
 const canvas = document.querySelector('.game-canvas');
 const ctx = canvas.getContext('2d');
 const blockTotal = 45;
+const blockArray = [];
 
 // Event Listeners
 rulesBtn.addEventListener('click', toggleRules);
 closeRulesBtn.addEventListener('click', toggleRules);
+window.addEventListener('keydown', setPlayerDirection);
+window.addEventListener('keyup', () => {
+  player.speed = 0;
+});
+
+// Determine direction of the player
+function setPlayerDirection(e) {
+  if (e.key === 'ArrowRight') {
+    player.speed = 5;
+  } else if (e.key === 'ArrowLeft') {
+    player.speed = -5;
+  }
+}
 
 // Toggle rules
 function toggleRules() {
@@ -20,16 +34,59 @@ const blockGap = 10;
 const blockWidth = (canvas.width - blockGap * 8 - canvasPad * 2) / 9;
 const blockHeight = 20;
 
+// Instantiate Player Bar
+const player = {
+  speed: 0,
+  height: 10,
+  width: 80,
+  y: canvas.height - 25,
+  x: canvas.width / 2 - 80 / 2,
+};
+
+// Draw Player
+function drawPlayer() {
+  movePlayer();
+  ctx.beginPath();
+  ctx.rect(player.x, player.y, player.width, player.height);
+  ctx.fillStyle = '#87a352';
+  ctx.fill();
+}
+
+// Update Player
+function movePlayer() {
+  player.x += player.speed;
+  if (player.x <= 0) {
+    player.x = 0;
+  } else if (player.x >= canvas.width - player.width) {
+    player.x = canvas.width - player.width;
+    console.log(player.x);
+  }
+}
+
+// Update Game
+function updateGame() {
+  clear();
+  drawPlayer();
+  drawAllBlocks();
+  requestAnimationFrame(updateGame);
+}
+
+// Clear Canvas
+function clear() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
 // Draw All Block
 function drawAllBlocks() {
   for (let i = 0; i < 45; i++) {
     const column = i % 9;
     const row = Math.floor(i / 9);
-    console.log(`Column: ${column}, Row: ${row}`);
+    // console.log(`Column: ${column}, Row: ${row}`);
     drawBlock(column, row);
   }
 }
-
+updateGame();
+drawPlayer();
 drawAllBlocks();
 
 // Draw Single Block
@@ -46,13 +103,3 @@ function drawBlock(column, row) {
   ctx.fillStyle = '#87a352';
   ctx.fill();
 }
-
-// drawBlock(0);
-// drawBlock(1);
-// drawBlock(2);
-// drawBlock(3);
-// drawBlock(4);
-// drawBlock(5);
-// drawBlock(6);
-// drawBlock(7);
-// drawBlock(8);
